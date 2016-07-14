5cbafdc02105752044b01428b7b2dbcd5a93ce54
lexville <alex.kangethe@andela.com>
2016-07-14 12:16:24 +0300

    Add user table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 10)->create();
+
+        factory(App\User::class)->create([
+            'name'     => 'userOne',
+            'email'    => 'userOne@email.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
